35c67288981b8ff15e78017c44dc39febf87490f
jameshih <j@shih.app>
2020-12-31 18:33:07 -0500

    update

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Content } from '../../core';
 
 export default function Home() {
     const [options] = useState(['otf', 'ttf', 'woff', 'woff2', 'eot']);
@@ -55,49 +54,45 @@ export default function Home() {
     }
 
     return (
-        <Content className='pt-20'>
-            <div className='lg:w-1/2 lg:mx-auto'>
-                <div className='flex justify-between'>
-                    <input
-                        className='w-3/5 pl-4 border border-gray-700'
-                        type='text'
-                        value={inputVal}
-                        onChange={handleChange}
-                        placeholder='font name'
-                    />
-                    <button className='w-1/3 border border-gray-800' onClick={handleClick}>
-                        Search
-                    </button>
-                </div>
-                <div className='flex mt-4 bg-gray-100 justify-evenly'>
-                    {options.map((elm, index) => (
-                        <div key={index}>
-                            <input
-                                type='checkbox'
-                                id={`${elm}_checkbox`}
-                                value={elm}
-                                onChange={handleCheck}
-                                defaultChecked={checked[elm]}
-                            />
-                            <label className='ml-2' htmlFor={`${elm}_checkbox`}>
-                                {elm}
-                            </label>
+        <div className='pt-24 lg:w-1/2 lg:mx-auto'>
+            <div className='flex justify-between'>
+                <input
+                    className='w-3/5 pl-4 border border-gray-700'
+                    type='text'
+                    value={inputVal}
+                    onChange={handleChange}
+                    placeholder='font name'
+                />
+                <button className='w-1/3 border border-gray-800' onClick={handleClick}>
+                    Search
+                </button>
+            </div>
+            <div className='flex mt-4 bg-gray-100 justify-evenly'>
+                {options.map((elm, index) => (
+                    <div key={index}>
+                        <input
+                            type='checkbox'
+                            id={`${elm}_checkbox`}
+                            value={elm}
+                            onChange={handleCheck}
+                            defaultChecked={checked[elm]}
+                        />
+                        <label className='ml-2' htmlFor={`${elm}_checkbox`}>
+                            {elm}
+                        </label>
+                    </div>
+                ))}
+            </div>
+            {loading && <div className='mt-8'>Searching...</div>}
+            {searchResult.length > 0 && (
+                <div className='mt-8'>
+                    {searchResult.map((elm: any, index: number) => (
+                        <div className='mt-4' key={elm.name + elm.repository.id + index}>
+                            <a href={`${elm.html_url.replace('/blob/', '/raw/')}`}>{elm.name}</a>
                         </div>
                     ))}
                 </div>
-                {loading && <div className='mt-8'>Searching...</div>}
-                {searchResult.length > 0 && (
-                    <div className='mt-8'>
-                        {searchResult.map((elm: any, index: number) => (
-                            <div className='mt-4' key={elm.name + elm.repository.id + index}>
-                                <a href={`${elm.html_url.replace('/blob/', '/raw/')}`}>
-                                    {elm.name}
-                                </a>
-                            </div>
-                        ))}
-                    </div>
-                )}
-            </div>
-        </Content>
+            )}
+        </div>
     );
 }
